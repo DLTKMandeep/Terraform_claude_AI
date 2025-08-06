54571fce4c76bad6944227fee6ee8de9d35abc18
DLTKMandeep <singh.mandeep@gmail.com>
2025-08-05 21:41:24 -0400

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_id" {
-  value       = random_string.cluster_id.result
-  description = "The generated cluster ID"
-}
-
-output "cluster_name" {
-  value       = random_pet.cluster_name.id
-  description = "The generated cluster name"
-}
